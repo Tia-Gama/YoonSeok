cf211043e0a46bff8bb89017aee87f6040afca2d
TiaGama <margo.avons@yandex.ru>
2021-12-24 12:06:53 +0700

    умножайся

--- Lab4.2/Program.cs
+++ Lab4.2/Program.cs
@@ -10,6 +10,10 @@ namespace Lab4._2
     {
         static void Main(string[] args)
         {
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+
+            int f = a * b;
         }
     }
 }
